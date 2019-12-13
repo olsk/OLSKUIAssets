e61db7dbf6c412bfba8955c92bd7b52f42d5b2aa
Rosano Coutinho <pub@xeari.com>
2019-12-12 19:25:27 -0500

    Use function notation

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const mod = {
 
-	OLSKUIAssetsSwapTokens: {
+	OLSKUIAssetsSwapTokens () {
 		'xmlns:xlink=\\"http://www.w3.org/1999/xlink\\" width=\\"16\\" height=\\"16\\"  xml:space=\\"preserve\\"': 'xmlns:xlink=\\"http://www.w3.org/1999/xlink\\" width=\\"16\\" height=\\"16\\" viewBox=\\"0 0 16 16\\"  xml:space=\\"preserve\\"',
 	},
 
