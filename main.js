23a668b680c0552fc7bb0f8341bbdc8babc32acd
Rosano Coutinho <pub@xeari.com>
2019-12-12 19:23:12 -0500

    Fix typo

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const mod = {
 
-	OLSKRollupPluginSwapTokens: {
+	OLSKUIAssetsSwapTokens: {
 		'xmlns:xlink=\\"http://www.w3.org/1999/xlink\\" width=\\"16\\" height=\\"16\\"  xml:space=\\"preserve\\"': 'xmlns:xlink=\\"http://www.w3.org/1999/xlink\\" width=\\"16\\" height=\\"16\\" viewBox=\\"0 0 16 16\\"  xml:space=\\"preserve\\"',
 	},
 
