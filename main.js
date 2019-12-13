cda9d5c209956cc14cbf79aa067056a40a772391
Rosano Coutinho <pub@xeari.com>
2019-12-12 19:22:59 -0500

    Define OLSKUIAssetsSwapTokens

---
+++ main.js
@@ -0,0 +1,9 @@
+const mod = {
+
+	OLSKRollupPluginSwapTokens: {
+		'xmlns:xlink=\\"http://www.w3.org/1999/xlink\\" width=\\"16\\" height=\\"16\\"  xml:space=\\"preserve\\"': 'xmlns:xlink=\\"http://www.w3.org/1999/xlink\\" width=\\"16\\" height=\\"16\\" viewBox=\\"0 0 16 16\\"  xml:space=\\"preserve\\"',
+	},
+
+};
+
+Object.assign(exports, mod);
