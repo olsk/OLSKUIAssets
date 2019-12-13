eb04607f5694299f522b361a1ecd9b439036f739
Rosano Coutinho <pub@xeari.com>
2019-12-12 19:26:57 -0500

    Fix typo

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const mod = {
 
 	OLSKUIAssetsSwapTokens () {
-		'xmlns:xlink=\\"http://www.w3.org/1999/xlink\\" width=\\"16\\" height=\\"16\\"  xml:space=\\"preserve\\"': 'xmlns:xlink=\\"http://www.w3.org/1999/xlink\\" width=\\"16\\" height=\\"16\\" viewBox=\\"0 0 16 16\\"  xml:space=\\"preserve\\"',
+		return {
+			'xmlns:xlink=\\"http://www.w3.org/1999/xlink\\" width=\\"16\\" height=\\"16\\"  xml:space=\\"preserve\\"': 'xmlns:xlink=\\"http://www.w3.org/1999/xlink\\" width=\\"16\\" height=\\"16\\" viewBox=\\"0 0 16 16\\"  xml:space=\\"preserve\\"',
+		};
 	},
 
 };
