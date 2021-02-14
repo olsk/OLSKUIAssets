c967c11b9ecfc68dac48354547660804b2c47d67
Rosano Coutinho <pub@xeari.com>
2021-02-14 13:37:44 -0300

    Define browser

---
+++ browser.js
@@ -0,0 +1,87 @@
+import _OLSKSharedFeatureCode from './_OLSKSharedFeatureCode.svg';
+import _OLSKInputClear from './_OLSKInputClear.svg';
+import _OLSKSharedAbout from './_OLSKSharedAbout.svg';
+import _OLSKSharedAndroidMore from './_OLSKSharedAndroidMore.svg';
+import _OLSKSharedApropos from './_OLSKSharedApropos.svg';
+import _OLSKSharedBack from './_OLSKSharedBack.svg';
+import _OLSKSharedClone from './_OLSKSharedClone.svg';
+import _OLSKSharedCloud from './_OLSKSharedCloud.svg';
+import _OLSKSharedCreate from './_OLSKSharedCreate.svg';
+import _OLSKSharedDiscard from './_OLSKSharedDiscard.svg';
+import _OLSKSharedEverywhere from './_OLSKSharedEverywhere.svg';
+import _OLSKSharedFeatureAccessible from './_OLSKSharedFeatureAccessible.svg';
+import _OLSKSharedFeatureArchive from './_OLSKSharedFeatureArchive.svg';
+import _OLSKSharedFeatureBookmarklet from './_OLSKSharedFeatureBookmarklet.svg';
+import _OLSKSharedFeatureBrackets from './_OLSKSharedFeatureBrackets.svg';
+import _OLSKSharedFeatureEverywhere from './_OLSKSharedFeatureEverywhere.svg';
+import _OLSKSharedFeatureExtension from './_OLSKSharedFeatureExtension.svg';
+import _OLSKSharedFeatureIO from './_OLSKSharedFeatureIO.svg';
+import _OLSKSharedFeatureJump from './_OLSKSharedFeatureJump.svg';
+import _OLSKSharedFeatureJump2 from './_OLSKSharedFeatureJump2.svg';
+import _OLSKSharedFeatureKeyboard from './_OLSKSharedFeatureKeyboard.svg';
+import _OLSKSharedFeatureOffline from './_OLSKSharedFeatureOffline.svg';
+import _OLSKSharedFeaturePrivate from './_OLSKSharedFeaturePrivate.svg';
+import _OLSKSharedFeaturePublish from './_OLSKSharedFeaturePublish.svg';
+import _OLSKSharedFeatureRecord from './_OLSKSharedFeatureRecord.svg';
+import _OLSKSharedFeatureSync from './_OLSKSharedFeatureSync.svg';
+import _OLSKSharedFeatureTags from './_OLSKSharedFeatureTags.svg';
+import _OLSKSharedFeatureTTS from './_OLSKSharedFeatureTTS.svg';
+import _OLSKSharedFeatureURL from './_OLSKSharedFeatureURL.svg';
+import _OLSKSharedFeatureZeroData from './_OLSKSharedFeatureZeroData.svg';
+import _OLSKSharedInfo from './_OLSKSharedInfo.svg';
+import _OLSKSharedInternational from './_OLSKSharedInternational.svg';
+import _OLSKSharediOSA2HS from './_OLSKSharediOSA2HS.svg';
+import _OLSKSharediOSShare from './_OLSKSharediOSShare.svg';
+import _OLSKSharedLanguage from './_OLSKSharedLanguage.svg';
+import _OLSKSharedLauncher from './_OLSKSharedLauncher.svg';
+import _OLSKSharedLock from './_OLSKSharedLock.svg';
+import _OLSKSharedReload from './_OLSKSharedReload.svg';
+import _OLSKSharedStorageDisconnect from './_OLSKSharedStorageDisconnect.svg';
+import _OLSKSharedSyncStart from './_OLSKSharedSyncStart.svg';
+import _OLSKSharedSyncStop from './_OLSKSharedSyncStop.svg';
+
+export default {
+
+	_OLSKSharedFeatureCode,
+	_OLSKInputClear,
+	_OLSKSharedAbout,
+	_OLSKSharedAndroidMore,
+	_OLSKSharedApropos,
+	_OLSKSharedBack,
+	_OLSKSharedClone,
+	_OLSKSharedCloud,
+	_OLSKSharedCreate,
+	_OLSKSharedDiscard,
+	_OLSKSharedEverywhere,
+	_OLSKSharedFeatureAccessible,
+	_OLSKSharedFeatureArchive,
+	_OLSKSharedFeatureBookmarklet,
+	_OLSKSharedFeatureBrackets,
+	_OLSKSharedFeatureEverywhere,
+	_OLSKSharedFeatureExtension,
+	_OLSKSharedFeatureIO,
+	_OLSKSharedFeatureJump,
+	_OLSKSharedFeatureJump2,
+	_OLSKSharedFeatureKeyboard,
+	_OLSKSharedFeatureOffline,
+	_OLSKSharedFeaturePrivate,
+	_OLSKSharedFeaturePublish,
+	_OLSKSharedFeatureRecord,
+	_OLSKSharedFeatureSync,
+	_OLSKSharedFeatureTags,
+	_OLSKSharedFeatureTTS,
+	_OLSKSharedFeatureURL,
+	_OLSKSharedFeatureZeroData,
+	_OLSKSharedInfo,
+	_OLSKSharedInternational,
+	_OLSKSharediOSA2HS,
+	_OLSKSharediOSShare,
+	_OLSKSharedLanguage,
+	_OLSKSharedLauncher,
+	_OLSKSharedLock,
+	_OLSKSharedReload,
+	_OLSKSharedStorageDisconnect,
+	_OLSKSharedSyncStart,
+	_OLSKSharedSyncStop,
+
+};
